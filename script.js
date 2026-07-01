da11aaa9c7f7c0c14391b5f5630f62843777f2be
italijancic08 <152649783+italijancic08@users.noreply.github.com>
2026-07-01 00:37:58 +0000

    feat: script y array de consejos

---
+++ script.js
@@ -0,0 +1,9 @@
+const consejos = [
+    "Lleva siempre comida antes de comenzar el ascenso.",
+    "No gastes toda tu resistencia en una sola subida.",
+    "Explora rutas alternativas para evitar zonas peligrosas.",
+    "Usa la cuerda cuando el terreno sea muy difícil.",
+    "Descansa antes de intentar una pared complicada.",
+    "Guarda las bebidas energéticas para emergencias.",
+    "Observa el terreno antes de comenzar a escalar."
+];
